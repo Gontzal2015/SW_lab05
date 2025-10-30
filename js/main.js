eb43ab379609b0ce70b9fa3516dee5b49ac4671e
Gontzal2015 <116571852+Gontzal2015@users.noreply.github.com>
2025-10-30 13:42:34 +0000

    comit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+import {SpriteSheet} from "./SpriteSheet.js";
+
+let lienzo = document.createElement('canvas')
+
+lienzo.style.width = 600
+lienzo.style.height = 200
+document.body.appendChild(lienzo)
+lienzo.style.border = '1px black solid'
